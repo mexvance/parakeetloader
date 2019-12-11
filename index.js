89e2167c8c7d4e22e227cde70b49d37cee9f45d1
Mike <mexvance04@gmail.com>
2019-12-11 13:42:37 -0700

    added header functionality and separated requests into functions

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const fetch = require('node-fetch');
 const dotenv = require('dotenv');
 dotenv.config();
-const port = process.env.PORT;
+
 const size = process.env.SIZE;
 const startingnumber = process.env.STARTINGNUMBER;
 const batchsize = process.env.BATCHSIZE;
 const url = process.env.URL;
 
   console.log("Project Parakeet, Let's get your request!");
-  fetch(`http://www.gutenberg.org/cache/epub/${startingnumber}/pg${batchsize}.txt`)
+  getBook(`http://www.gutenberg.org/cache/epub/${startingnumber}/pg${batchsize}.txt`);
+
+function getHeader(body){
+  var array = body.split("\r\n");
+  for(var i = 0; i < 20; i++){
+      console.log(`loop ${i}`)
+      if(array[i].includes("Project Gutenberg"))
+      {
+        return array[i]
+      }
+  }
+  return "";
+}
+async function getBook(request){
+    fetch(request)
       .then(res => res.text())
       .then(body => {
           let book = {}
-          realbody =  body.slice(1,size);
-          book.text = realbody
-          book.title = getheader(book.text);
-          jsonbook = JSON.stringify(book);
-          console.log(book)
-
+          book.title = getHeader(body);
+          book.text =  body.slice(0,size);
+          jsonBook = JSON.stringify(book);
           fetch(`${url}`,{
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json'
               },
               mode: 'cors',
-              body: jsonbook
+              body: jsonBook
             }
             ).then(response=>{
               console.log(response)
             })
-            .catch(error=>Promise.reject());
       });
-
-
-function getheader(body){
-  return "Kylers";
 }
